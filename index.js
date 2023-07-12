4af1fab1772d398c8b68958bc24b9a5590bbf6d2
Sourabh Patel <sourabhpatel073@gmail.com>
2023-07-12 23:58:14 +0530

    next

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use("/",(req,res)=>{
+res.send("hello")
+})
 app.use("/user", userRouter);
 app.use(auth);
 app.use("/notes", noteRouter);
